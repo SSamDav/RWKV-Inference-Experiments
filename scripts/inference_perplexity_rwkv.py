ae88e4b658f47c2f17bdaca83dfc5612f80ec1d5
SSamDav <ssam_dav@hotmail.com>
2023-10-15 21:47:39 +0100

    Improve cross entropy computation

--- scripts/inference_perplexity_rwkv.py
+++ scripts/inference_perplexity_rwkv.py
@@ -3,10 +3,8 @@ from tokenizers import Tokenizer
 from rwkv.model import RWKV
 from huggingface_hub import hf_hub_download
 from pathlib import Path
-from torch import nn
 import torch.nn.functional as F
 from tqdm.auto import tqdm
-from torchmetrics.text import Perplexity
 
 import requests
 import torch
@@ -18,9 +16,9 @@ models = [
     # "BlinkDL/rwkv-4-pile-169m",
     # "BlinkDL/rwkv-4-pile-430m",
     # "BlinkDL/rwkv-4-pile-1b5",
-    "BlinkDL/rwkv-4-pile-3b",
-    # "BlinkDL/rwkv-4-pile-7b",
-    # "BlinkDL/rwkv-4-pile-14b",
+    # "BlinkDL/rwkv-4-pile-3b",
+    "BlinkDL/rwkv-4-pile-7b",
+    "BlinkDL/rwkv-4-pile-14b",
 ]
 
 model_mapping = {
@@ -28,14 +26,15 @@ model_mapping = {
     "BlinkDL/rwkv-4-pile-430m": "RWKV-4-Pile-430M-20220808-8066.pth",
     "BlinkDL/rwkv-4-pile-1b5": "RWKV-4-Pile-1B5-20220903-8040.pth",
     "BlinkDL/rwkv-4-pile-3b": "RWKV-4-Pile-3B-20221008-8023.pth",
-    "BlinkDL/rwkv-4-pile-7b": "RWKV-4-Pile-7B-20221115-8047.pth",
-    "BlinkDL/rwkv-4-pile-14b": "RWKV-4-Pile-14B-20230213-8019.pth",
+    "BlinkDL/rwkv-4-pile-7b": "RWKV-4-Pile-7B-20230406-ctx8192-test949.pth",
+    "BlinkDL/rwkv-4-pile-14b": "RWKV-4-Pile-14B-20230313-ctx8192-test1050.pth",
 
 }
 
 
 def tokenize(example):
-    return {"ids": tokenizer.encode(example["text"]).ids}
+    ids = tokenizer.encode(example["text"]).ids
+    return {"ids": ids[:128000], "length": len(ids)}
 
 
 def calculate_perplexity(logits, targets):
@@ -53,9 +52,8 @@ tokenizer = Tokenizer.from_file("20B_tokenizer.json")
 
 # Loading Dataset
 dataset = load_dataset('hoskinson-center/proof-pile', split='train', streaming=True)
-tokenized_dataset = dataset.map(tokenize)
+tokenized_dataset = dataset.map(tokenize).filter(lambda example: example['length'] >= 128000)
 
-metric = Perplexity(ignore_index=1)
 
 for strategy in strategies:
     for model_name in models:
@@ -65,28 +63,29 @@ for strategy in strategies:
             
         model_weights = Path(model_name) / model_mapping[model_name]
         model = RWKV(model=model_weights.as_posix(), strategy=strategy)
-        
-        with open(f"perplexity_by_context_{processed_name}.jsonl", "w") as fp:
-            state, previous_token, context_length = None, None, 0
-            for doc_id, doc in enumerate(tokenized_dataset):
-                if context_length >= 128000: break
-                for token in tqdm(doc["ids"], leave=True, desc=f"Model: {model_name}, DocId: {doc_id}"):
-                    if previous_token is None:
+        with torch.no_grad():
+            with open(f"perplexity_by_context_{processed_name}.jsonl", "w") as fp:
+                for doc_id, doc in enumerate(tokenized_dataset):
+                    if doc_id >= 10: break
+
+                    state, previous_token = None, None
+                    for token_id, token in enumerate(tqdm(doc["ids"], leave=True)):
+                        if token_id == 0:
+                            previous_token = token
+                            continue
+
+                        if state is not None:
+                            state = [s.to(strategy.split()[0]) for s in state]
+
+                        token_tensor = torch.tensor([previous_token])
+                        output, state = model.forward(token_tensor, state=state)
+                        cross_entropy = F.cross_entropy(output.cpu().unsqueeze(0), torch.tensor([token])).tolist()
+                        json.dump(
+                            {
+                                "context_length": token_id,
+                                "cross_entropy": cross_entropy,
+                                "doc_id": doc_id
+                            }, fp
+                        )
+                        fp.write("\n")
                         previous_token = token
-                        context_length += 1
-                        continue
-
-                    if state is not None:
-                        state = [s.to(strategy.split()[0]) for s in state]
-
-                    token_tensor = torch.tensor([previous_token])
-                    output, state = model.forward(token_tensor, state=state)
-                    cross_entropy = F.cross_entropy(output.cpu().unsqueeze(0), torch.tensor([token])).tolist()
-                    json.dump({
-                            "context_length": context_length,
-                            "cross_entropy": cross_entropy
-                        }, fp)
-                    fp.write("\n")
-                    
-                    previous_token = token
-                    context_length += 1
